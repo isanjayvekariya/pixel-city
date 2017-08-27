ca28edc1af49d360615747e1013f2c3fdfdf3f19
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-08-27 11:17:29 -0400

    requesting location services

--- Pixel-City/Controller/MapVC.swift
+++ Pixel-City/Controller/MapVC.swift
@@ -8,23 +8,50 @@
 
 import UIKit
 import MapKit
-
+import CoreLocation
 
 class MapVC: UIViewController {
     
     // Outlets
     @IBOutlet weak var mapView: MKMapView!
     
+    var locationManager = CLLocationManager()
+    var authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionradius: Double = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
     }
 
     @IBAction func centerMapBtnPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
 }
 
 extension MapVC: MKMapViewDelegate {
+    func centerMapOnUserLocation () {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionradius * 2.0, regionradius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
     
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
 }
 
