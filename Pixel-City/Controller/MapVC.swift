1a75d73bb8465c93b2ddea958904bf4a7704b939
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-08-27 10:49:33 -0400

    MapVC UI and delegate

--- Pixel-City/Controller/MapVC.swift
+++ Pixel-City/Controller/MapVC.swift
@@ -7,21 +7,24 @@
 //
 
 import UIKit
-import Alamofire
+import MapKit
 
 
 class MapVC: UIViewController {
-
+    
+    // Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func centerMapBtnPressed(_ sender: Any) {
     }
+}
 
-
+extension MapVC: MKMapViewDelegate {
+    
 }
 
